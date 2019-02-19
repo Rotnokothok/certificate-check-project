1184333e8f846aebc0465911e8069cb54703269b
Rotnokothok <Rotnokothok@gmail.com>
2019-02-19 22:31:10 +0600

    Site Add in first time

---
+++ js/customscript.js
@@ -0,0 +1,65 @@
+
+var clickBtn = document.getElementById('chk');
+clickBtn.onclick = function demo(){
+var x = document.getElementById('data').value;   
+if (x==201601){
+        var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/arif_201601.png');   
+    }else if(x==201602){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/shofikul_201602.png');
+    }else if(x==201603){
+        var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/jamil_201603.png');
+    }else if(x==201604){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/.png');
+    }else if(x==201605){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/tareq_201605.png');
+    }else if(x==201606){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/rasel_201606.png');
+    }else if(x==201607){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/rashed_201607.png');
+    }else if(x==201608){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/saiful_201608.png');
+    }else if(x==201609){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/mohibul_201609.png');
+    }else if(x==201610){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/hasan_201610.png');
+    }else if(x==201701){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/korim_201701.png');
+    }else if(x==201702){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/antor_201702.png');
+    }else if(x==201703){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/sonia_201703.png');
+    }else if(x==201704){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/saifulj_201704.png');
+    }else if(x==201705){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/sohel_201705.png');
+    }else if(x==201706){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/shamim_201706.png');
+    }else if(x==201707){
+       var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/kalam_201707.png');
+    
+    }else{
+        var contentArea = document.getElementById('CfPlace');
+            contentArea.setAttribute('src','file/wrong.png');    
+}
+};
+
+
+
+
